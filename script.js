62901277e8a142fa87f0d21c9794c3f7211b70d0
Marwa Mohammed <52058731+MarwaElasuty@users.noreply.github.com>
2024-05-19 00:03:10 +0300

    dsaew

--- script.js
+++ script.js
@@ -28,3 +28,35 @@ tabs.forEach((tab, index) => {
 
 // SLIDER
 
+const sliderImages = document.querySelector('.slider-images');
+const images = document.querySelectorAll('.slider-images img'); 
+const overlay = document.querySelector('.overlay');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+let currentIndex = 0;
+const totalImages = images.length;
+const imageWidth = 1350; 
+
+function showSlide(index) {
+  overlay.style.left = '0';
+  setTimeout(() => {
+    overlay.style.left = '100%';
+    setTimeout(() => {
+      sliderImages.style.transform = `translateX(${-index * imageWidth}px)`; 
+      setTimeout(() => {
+        overlay.style.left = '-100%';
+      }, 700); 
+    }, 600); 
+  }, 0);
+}
+
+nextButton.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % totalImages;
+  showSlide(currentIndex);
+});
+
+prevButton.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+  showSlide(currentIndex);
+});
